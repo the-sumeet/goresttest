4dc32d51c5732cf40f9197d67f753cb587ac9bb0
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-04-03 15:20:59 +0530

    test validation field parsing

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,29 +1,43 @@
 package config
 
 import (
+	"reflect"
 	"testing"
-
-	"gopkg.in/yaml.v3"
 )
 
 func TestLoadTestCases(t *testing.T) {
+
+	expectedData := []TestCase{
+		{
+			Name:   "Get public API",
+			URL:    "https://jsonplaceholder.typicode.com",
+			Method: "GET",
+		},
+		{
+			Name:   "Test invalid endpoint",
+			URL:    "https://jsonplaceholder.typicode.com/foo",
+			Method: "GET",
+			Validation: Validation{
+				StatusCode: "4",
+			},
+		},
+		{
+			Name:   "Test invalid endpoint",
+			URL:    "https://jsonplaceholder.typicode.com/foo",
+			Method: "GET",
+			Validation: Validation{
+				StatusCode: "404",
+			},
+		},
+	}
 
 	testData, err := LoadTestCases("test.yml")
 	if err != nil {
 		t.Fatalf("Failed to load test cases: %v", err)
 	}
 
-	if len(testData) != 2 {
-		t.Fatalf("Expected 2 test cases, got %d", len(testData))
-	}
-
-	testDataSerialized, err := yaml.Marshal(testData)
-	if err != nil {
-		t.Fatalf("Failed to serialize test cases: %v", err)
-	}
-
-	if string(testDataSerialized) != "- name: Get public API\n  url: https://api.publicapis.org/entries\n  method: GET\n  headers: {}\n  body: \"\"\n- name: Test invalid endpoint\n  url: https://api.publicapis.org/invalid\n  method: GET\n  headers: {}\n  body: \"\"\n" {
-		t.Fatalf("Serialized test cases do not match expected output")
+	if !reflect.DeepEqual(testData, expectedData) {
+		t.Fatalf("Expected %v, got %v", expectedData, testData)
 	}
 
 }
