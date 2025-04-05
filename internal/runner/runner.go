c46aa27c39773db16227ef83e508ab87aabb23f1
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-04-05 18:05:22 +0530

    validation support in runner

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -2,10 +2,17 @@ package runner
 
 import (
 	"bytes"
+	"errors"
 	"goresttest/internal/config"
+	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
+
+	"github.com/tidwall/gjson"
 )
 
 type TestResult struct {
@@ -15,14 +22,25 @@ type TestResult struct {
 	Error      error
 }
 
-func RunTest(testCase config.TestCase) TestResult {
-	result := TestResult{Passed: true, TestCase: testCase}
+func RunTest(testCase config.TestCase) (result TestResult) {
+
+	data := map[string]interface{}{}
+	getEnvVars(data)
+
+	result = TestResult{Passed: false, TestCase: testCase}
 
 	client := &http.Client{}
+
+	testCase, err := interpolateVariables(testCase, data)
+	if err != nil {
+		result.Error = err
+		return
+	}
+
 	req, err := http.NewRequest(testCase.Method, testCase.URL, bytes.NewBufferString(testCase.Body))
 	if err != nil {
 		result.Error = err
-		return result
+		return
 	}
 
 	// Set headers
@@ -39,14 +57,89 @@ func RunTest(testCase config.TestCase) TestResult {
 
 	result.StatusCode = resp.StatusCode
 
-	// Validate statuc code
-	if testCase.Validation.StatusCode == "" {
-		testCase.Validation.StatusCode = "2"
+	err = performValidation(testCase, resp)
+	if err != nil {
+		result.Error = err
+		return result
 	}
 
-	if !strings.HasPrefix(strconv.Itoa(resp.StatusCode), testCase.Validation.StatusCode) {
-		result.Passed = false
-	}
+	// // Validate statuc code
+	// if testCase.Validation.StatusCode == "" {
+	// 	testCase.Validation.StatusCode = "2"
+	// }
 
+	// if !strings.HasPrefix(strconv.Itoa(resp.StatusCode), testCase.Validation.StatusCode) {
+	// 	return
+	// }
+
+	result.Passed = true
 	return result
+}
+
+func performValidation(testCase config.TestCase, resp *http.Response) error {
+
+	for _, validation := range testCase.Validation {
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+
+		// Validate status code
+		statusCodeToCheck := validation.StatusCode
+		if statusCodeToCheck == "" {
+			statusCodeToCheck = "2"
+		}
+
+		if !strings.HasPrefix(strconv.Itoa(resp.StatusCode), statusCodeToCheck) {
+			return errors.New("status code does not match")
+		}
+
+		// Validate compare
+		jsonPath := validation.Compare.JSONPath
+		if jsonPath != "" {
+			value := gjson.Get(bodyString, jsonPath)
+			if !value.Exists() {
+				return errors.New("json path does not exist")
+			}
+
+			val := value.Value()
+			expceted := validation.Compare.Expected
+
+			if value.Type == gjson.Number {
+				expcetedInt, ok := expceted.(int)
+				if ok {
+					expceted = float64(expcetedInt)
+				}
+			}
+
+			if val != expceted {
+				return errors.New("json path does not match")
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func interpolateVariables(testCase config.TestCase, data map[string]interface{}) (config.TestCase, error) {
+	t := template.Must(template.New("doesntmatter").Parse(testCase.URL))
+	var finalUrl bytes.Buffer
+	err := t.Execute(&finalUrl, data)
+	if err != nil {
+		return testCase, err
+	}
+	testCase.URL = finalUrl.String()
+	return testCase, nil
+}
+
+func getEnvVars(data map[string]interface{}) {
+	for _, env := range os.Environ() {
+		keyValue := strings.SplitN(env, "=", 2)
+		if len(keyValue) == 2 {
+			data[keyValue[0]] = keyValue[1]
+		}
+	}
 }
--- internal/runner/runner_test.go
+++ internal/runner/runner_test.go
@@ -2,6 +2,7 @@ package runner
 
 import (
 	"goresttest/internal/config"
+	"os"
 	"testing"
 )
 
@@ -12,11 +13,14 @@ type TestCases struct {
 
 func TestRunTest(t *testing.T) {
 
+	// Set env vars
+	os.Setenv("goresttest_example", "example.com")
+
 	// Test cases
 	testCases := []TestCases{
 		{
 			testCase: config.TestCase{
-				Name:   "Test Case 1",
+				Name:   "Test Case Success",
 				URL:    "http://example.com",
 				Method: "GET",
 			},
@@ -26,11 +30,13 @@ func TestRunTest(t *testing.T) {
 		},
 		{
 			testCase: config.TestCase{
-				Name:   "Test Case 1",
+				Name:   "Test Case Not Found Partial Status",
 				URL:    "http://example.com/foo",
 				Method: "GET",
-				Validation: config.Validation{
-					StatusCode: "4",
+				Validation: []config.Validation{
+					{
+						StatusCode: "4",
+					},
 				},
 			},
 			expectedResult: TestResult{
@@ -39,11 +45,11 @@ func TestRunTest(t *testing.T) {
 		},
 		{
 			testCase: config.TestCase{
-				Name:   "Test Case 1",
+				Name:   "Test Case Not Found",
 				URL:    "http://example.com/foo",
 				Method: "GET",
-				Validation: config.Validation{
-					StatusCode: "404",
+				Validation: []config.Validation{
+					{StatusCode: "404"},
 				},
 			},
 			expectedResult: TestResult{
@@ -52,15 +58,67 @@ func TestRunTest(t *testing.T) {
 		},
 		{
 			testCase: config.TestCase{
-				Name:   "Test Case 1",
+				Name:   "Test Case Not Found Partial Invalid Status ",
 				URL:    "http://example.com/foo",
 				Method: "GET",
-				Validation: config.Validation{
-					StatusCode: "42",
+				Validation: []config.Validation{
+					{
+						StatusCode: "42",
+					},
 				},
 			},
 			expectedResult: TestResult{
 				Passed: false,
+			},
+		},
+		{
+			testCase: config.TestCase{
+				Name:   "Test templating",
+				URL:    "http://{{ .goresttest_example }}",
+				Method: "GET",
+				Validation: []config.Validation{
+					{
+						StatusCode: "200",
+					},
+				},
+			},
+			expectedResult: TestResult{
+				Passed: true,
+			},
+		},
+		{
+			testCase: config.TestCase{
+				Name:   "Test Templating Wrong Variable",
+				URL:    "http://{{ .goresttest_foo }}",
+				Method: "GET",
+				Validation: []config.Validation{
+					{
+						StatusCode: "200",
+					},
+				},
+			},
+			expectedResult: TestResult{
+				Passed: false,
+			},
+		},
+		{
+			testCase: config.TestCase{
+				Name:   "Test Validator",
+				URL:    "https://jsonplaceholder.typicode.com/todos/1",
+				Method: "GET",
+				Validation: []config.Validation{
+					{
+						StatusCode: "200",
+						Compare: config.Compare{
+							JSONPath:   "userId",
+							Comparator: "eq",
+							Expected:   1,
+						},
+					},
+				},
+			},
+			expectedResult: TestResult{
+				Passed: true,
 			},
 		},
 	}
